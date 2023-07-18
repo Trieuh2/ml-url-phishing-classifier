20ae89da1727ea1b52a788688e5e646535bfaddb
Henry Trieu <henrytrieu88@gmail.com>
2023-07-17 23:21:19 -0700

    Condensed feature correlation matrix functions into one function

--- data_visualization.py
+++ data_visualization.py
@@ -139,12 +139,12 @@ def visualize_numerical_feature_distribution(dataset_filepath, numerical_feature
     return None
 
 # Function to visualize binary feature correlation via heatmap
-def visualize_binary_feature_correlation(dataset_filepath, binary_features, target_header):
+def visualize_feature_correlation(dataset_filepath, feature_type, selected_features, target_header):
     # Load the dataset into a DataFrame
     df = load_data(dataset_filepath)
 
     # Drop all features that are not binary
-    df = df[binary_features + [target_header]]
+    df = df[selected_features + [target_header]]
 
     # Compute correlation matrix
     corr_matrix = df.corr()
@@ -153,7 +153,7 @@ def visualize_binary_feature_correlation(dataset_filepath, binary_features, targ
     plt.figure(figsize=(12, 6))
 
     # Set title
-    plt.title('Correlation Matrix of Binary Features')
+    plt.title('Correlation Matrix of ' + feature_type + ' Features')
 
     # Plot data
     corr_plot = sns.heatmap(corr_matrix, annot=True)
@@ -168,40 +168,6 @@ def visualize_binary_feature_correlation(dataset_filepath, binary_features, targ
     # Save the plot as a PNG image
     dataset_filename = dataset_filepath.split('/')[-1]
     dataset_file_prefix = dataset_filename.split('.')[0]
-    plt.savefig('visualizations/' + dataset_file_prefix + '_binary_feature_correlation.png', dpi=300)
-
-    return None
-
-# Function to visualize numerical feature correlation via heatmap
-def visualize_numerical_feature_correlation(dataset_filepath, numerical_features, target_header):
-    # Load the dataset into a DataFrame
-    df = load_data(dataset_filepath)
-
-    # Drop all features that are not binary
-    df = df[numerical_features + [target_header]]
-
-    # Compute correlation matrix
-    corr_matrix = df.corr()
-
-    # Create a large figure to allow enough space for the heatmap
-    plt.figure(figsize=(12, 6))
-
-    # Set title
-    plt.title('Correlation Matrix of Numerical Features')
-
-    # Plot data
-    corr_plot = sns.heatmap(corr_matrix, annot=True, cmap='RdBu')
-
-    # Rotate x-axis labels to horizontal and set tight layout to prevent overlapping labels
-    plt.xticks(rotation=45)
-    plt.tight_layout()
-
-    # Adjust subplot parameters to make more space at the bottom
-    plt.subplots_adjust(bottom=0.25)
-
-    # Save the plot as a PNG image
-    dataset_filename = dataset_filepath.split('/')[-1]
-    dataset_file_prefix = dataset_filename.split('.')[0]
-    plt.savefig('visualizations/' + dataset_file_prefix + '_numerical_feature_correlation.png', dpi=300)
+    plt.savefig('visualizations/' + dataset_file_prefix + '_' + feature_type + '_feature_correlation.png', dpi=300)
 
     return None
